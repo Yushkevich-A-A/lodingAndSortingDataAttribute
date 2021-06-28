d49acbcbad571557b4c082bedbb12f93fc87cc29
Yushkevich A.A. work computer <Mortifer1993@yandex.ru>
2021-06-28 11:13:27 +0300

    add  table

--- src/js/Table/Table.js
+++ src/js/Table/Table.js
@@ -2,9 +2,58 @@ export default class Table {
   constructor(data = null) {
     this.data = data;
     this.thead = document.querySelector('.thead');
-    this.tboby = document.querySelector('.tboby');
+    this.tbody = document.querySelector('.tbody');
+    this.index = 0;
+
+    this.sortParametr = [
+      {
+        param: 'id',
+        func: (a, b) => a.dataset.id - b.dataset.id,
+      },
+      {
+        param: 'id',
+        func: (a, b) => b.dataset.id - a.dataset.id,
+      },
+      {
+        param: 'title',
+        func: (a, b) => a.dataset.title > b.dataset.title,
+      },
+      {
+        param: 'title',
+        func: (a, b) => a.dataset.title < b.dataset.title,
+      },
+      {
+        param: 'imdb',
+        func: (a, b) => a.dataset.imdb - b.dataset.imdb,
+      },
+      {
+        param: 'imdb',
+        func: (a, b) => b.dataset.imdb - a.dataset.imdb,
+      },
+      {
+        param: 'year',
+        func: (a, b) => a.dataset.year - b.dataset.year,
+      },
+      {
+        param: 'yaer',
+        func: (a, b) => b.dataset.year - a.dataset.year,
+      },
+    ];
 
     this.drawTable();
+    this.sortInterval();
+    }
+
+    sortInterval() {
+      if (this.index === this.sortParametr.length) {
+        this.index = 0;
+      }
+      setTimeout(() => {
+        console.log(this.sortParametr);
+        this.sort(this.sortParametr[this.index].param, this.sortParametr[this.index].func);
+        this.index++;
+        this.sortInterval();
+      }, 2000);
     }
 
     drawTable() {
@@ -41,8 +90,19 @@ export default class Table {
           tr.appendChild(td);
         }
 
-        this.tboby.appendChild(tr);
+        this.tbody.appendChild(tr);
       }
+
+    }
+
+    sort( param, func = null) {
+      this.sortParametrs = Array.from(document.querySelectorAll(`[data-${param}]`));
+      
+      this.sortParametrs.sort(func);
+      for (let i of this.sortParametrs) {
+        this.tbody.append(i);
+      }
+
 
     }
 

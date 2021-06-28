263f4543451a5f23fc4a542c75832c3730aaba5a
Yushkevich-A-A <Mortifer1993@yandex.ru>
2021-06-28 22:34:17 +0300

    Table edited again

--- src/js/Table/Table.js
+++ src/js/Table/Table.js
@@ -23,15 +23,6 @@ export default class Table {
     if (this.data == null) {
       return;
     }
-    const trHead = document.createElement('tr');
-    const arrKeys = Object.keys(this.data[0]);
-    for (const i of arrKeys) {
-      const th = document.createElement('th');
-      th.dataset.thTitle = i;
-      th.textContent = i;
-      trHead.appendChild(th);
-    }
-    document.querySelector('.thead').appendChild(trHead);
 
     for (const item of this.data) {
       item.imdb = item.imdb.toFixed(2);

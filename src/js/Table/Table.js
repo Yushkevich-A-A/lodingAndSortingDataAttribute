ff3500444c4e4c31b02cfeeda8fb0a91444b3c7d
Yushkevich-A-A <Mortifer1993@yandex.ru>
2021-06-29 20:51:52 +0300

    add arrows

--- src/js/Table/Table.js
+++ src/js/Table/Table.js
@@ -1,7 +1,7 @@
 export default class Table {
   constructor(data = null) {
     this.data = data;
-    this.paramDataSet = Object.keys(this.data[0]);
+    this.paramDataSet = ['id','title','year', 'imdb'];
     this.index = 0;
 
     this.drawTable();
@@ -10,16 +10,26 @@ export default class Table {
   drawTable() {
     this.table = document.createElement('table');
     this.table.classList.add('table');
-    this.table.innerHTML = `<thead class="thead">
-                              <th>id</th>
-                              <th>title</th>
-                              <th>year</th>
-                              <th>imdb</th>
-                            </thead>  
+    this.table.innerHTML = `<thead class="thead"></thead>  
                             <tbody class="tbody"></tbody>`;
 
     document.body.appendChild(this.table);
 
+    this.thead = document.querySelector('.thead');
+
+    const trHead = document.createElement('tr');
+    for(let i of this.paramDataSet) {
+      const th = document.createElement('th');
+      th.dataset.columnType = i;
+      th.dataset.th = 'th';
+      th.textContent = i;
+      trHead.appendChild(th);
+    }
+
+    this.thead.appendChild(trHead);
+
+
+    
     if (this.data == null) {
       return;
     }
@@ -32,13 +42,13 @@ export default class Table {
                         <td>(${item.year})</td>
                         <td>imdb:${item.imdb}</td>`;
 
-      for (const i of Object.keys(item)) {
+      for (const i of this.paramDataSet) {
         trData.dataset[i] = item[i];
       }
       this.tbody = document.querySelector('.tbody');
       this.tbody.appendChild(trData);
     }
-    this.sortMethod();
+    this.sortTimeout();
   }
 
   drawTBody(sortData) {
@@ -47,42 +57,51 @@ export default class Table {
     }
   }
 
-  sortMethod() {
+  sortTimeout() {
     if (this.index === this.paramDataSet.length) {
       this.index = 0;
     }
-    const parametr = this.paramDataSet[this.index];
-    this.sortTimeout(parametr);
+    const param = this.paramDataSet[this.index];
+    const arr = Array.from(this.tbody.children);
+    setTimeout(() => {
+      arr.sort((a, b) => this.funcSort(a.dataset[param], b.dataset[param]));
+      this.drawTBody(arr);
+      this.drawArrow(param, true);
+      setTimeout(() => {
+        arr.reverse();
+        this.drawTBody(arr);
+        this.drawArrow(param);
+        this.index++;
+        this.sortTimeout();
+      }, 2000);
+    }, 2000);
   }
 
-  sortTimeout(param) {
-    const arr = Array.from(this.tbody.children);
-    const testVal = document.querySelector(`[data-${param}]`).dataset[param];
-    if (isNaN(parseFloat(testVal))) {
-      setTimeout(() => {
-        arr.sort((a, b) => ((a.dataset[param] > b.dataset[param]) ? 1
-          : (a.dataset[param] < b.dataset[param]) ? -1 : 0));
-        this.drawTBody(arr);
-        setTimeout(() => {
-          arr.sort((a, b) => ((a.dataset[param] > b.dataset[param]) ? -1
-            : (a.dataset[param] < b.dataset[param]) ? 1
-              : 0));
-          this.drawTBody(arr);
-          this.index++;
-          this.sortMethod();
-        }, 2000);
-      }, 2000);
+  funcSort(a, b) {
+  const valA = (isNaN(parseFloat(a))) ? a: parseFloat(a);
+  const valB = (isNaN(parseFloat(b))) ? b: parseFloat(b);
+  if (valA > valB) {
+    return 1;
+  }
+  if (valA < valB) {
+    return -1;
+  }
+  return 0;
+  }
+
+  drawArrow(column, arrowUp = false) {
+    const arrColumnTitle = document.querySelectorAll('[data-th="th"]');
+    for (let i of arrColumnTitle) {
+      i.classList.remove('increase');
+      i.classList.remove('decrease');
+    }
+
+    const columnElement = document.querySelector(`[data-column-type="${column}"]`);
+
+    if(arrowUp) {
+      columnElement.classList.add('decrease');
     } else {
-      setTimeout(() => {
-        arr.sort((a, b) => a.dataset[param] - b.dataset[param]);
-        this.drawTBody(arr);
-        setTimeout(() => {
-          arr.sort((a, b) => b.dataset[param] - a.dataset[param]);
-          this.drawTBody(arr);
-          this.index++;
-          this.sortMethod();
-        }, 2000);
-      }, 2000);
+      columnElement.classList.add('increase');
     }
   }
 }
